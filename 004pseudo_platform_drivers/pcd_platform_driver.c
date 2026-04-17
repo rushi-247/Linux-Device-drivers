110ce0ef1fe278525079b18e3804766f50f71ff1
RUSHIKESH PATIL <95530147+rushi-247@users.noreply.github.com>
2026-04-17 17:44:41 +0530

    Updated with devm_kzalloc

--- 004pseudo_platform_drivers/pcd_platform_driver.c
+++ 004pseudo_platform_drivers/pcd_platform_driver.c
@@ -99,10 +99,6 @@ void pcd_platform_driver_remove(struct platform_device *pdev)
 	/*2. Remove a cdev entry from the system*/
 	cdev_del(&dev_data->cdev);
 
-	/*3.Free the memory held by the device*/
-	kfree(dev_data->buffer);
-	kfree(dev_data);
-
 	pcdrv_data.total_devices--;
 	
 	pr_info("A device is removed\n");
@@ -129,7 +125,7 @@ int pcd_platform_driver_probe(struct platform_device *pdev)
 	}
 
 	/*2.Dynamically allocate memory for the device private bus*/
-	dev_data = kzalloc(sizeof(*dev_data), GFP_KERNEL);
+	dev_data = devm_kzalloc(&pdev->dev, sizeof(*dev_data), GFP_KERNEL);
 	if(!dev_data){
 		pr_info("Cannot allocate memory\n");
 		ret = -ENOMEM;
@@ -149,7 +145,7 @@ int pcd_platform_driver_probe(struct platform_device *pdev)
 
 	/*3.Dynamically allocate memory for the device buffer using size information
 	from the platform data */
-	dev_data->buffer = kzalloc(dev_data->pdata.size, GFP_KERNEL);
+	dev_data->buffer = devm_kzalloc(&pdev->dev, dev_data->pdata.size, GFP_KERNEL);
 	if(!dev_data->buffer){
 		pr_info("Cannot allocate memory\n");
 		ret = -ENOMEM;
@@ -187,9 +183,9 @@ int pcd_platform_driver_probe(struct platform_device *pdev)
 cdev_del:
 	cdev_del(&dev_data->cdev);
 buffer_free:
-	kfree(dev_data->buffer);
+	devm_kfree(&pdev->dev, dev_data->buffer);
 dev_data_free:
-	kfree(dev_data);
+	devm_kfree(&pdev->dev, dev_data);
 out:
 	pr_info("Device probing failed\n");
 	return ret;
